c6e653306037d9781c2deb03454da47cacdccc31
Méric Fournier <meric.fournier@hetic.net>
2018-06-01 17:22:53 +0200

    initial commit

---
+++ app/src/main/java/fr/mericfournier/unesco/homeActivity.kt
@@ -0,0 +1,15 @@
+package fr.mericfournier.unesco
+
+import android.graphics.Typeface
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.content.res.ResourcesCompat
+
+class homeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
+}
